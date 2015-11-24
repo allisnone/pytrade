f07c2d65d681b668d1ae63057cfe930db5ffab68
Administrator <Administrator@USER-20150725QL>
2015-11-24 23:58:45 +0800

    update

--- pdsql.py
+++ pdsql.py
@@ -6,6 +6,9 @@ import numpy as np
 import tushare as ts
 from pandas.io import sql
 from tradeStrategy import *
+from pandas.lib import to_datetime
+from pandas.lib import Timestamp
+import datetime
 
 def form_sql(table_name,oper_type='query',select_field=None,where_condition=None,insert_field=None,update_field=None,update_value=None):
     """
@@ -14,7 +17,7 @@ def form_sql(table_name,oper_type='query',select_field=None,where_condition=None
     :param where_condition: string type, like 'field_value>50'
     :param insert_field: string type, like '(date_time,measurement_id,value)'
     :param update_field: string type, like 'value' or  '(measurement_id,value)'
-    :param update_value: string type, like '1000' or "'normal_type'"
+    :param update_value: value or string type, like '1000' or "'normal_type'"
     :return: sql string
     
     :use example:
@@ -39,7 +42,12 @@ def form_sql(table_name,oper_type='query',select_field=None,where_condition=None
         value_tail='%s,'*num
         value_tail='('+value_tail[:-1]+')'
         sql='insert into %s '% table_name +insert_field +' values'+ value_tail + ';'
-    elif oper_type=='update' and where_condition and update_field and update_value:
+    elif oper_type=='update' and where_condition and update_field:
+        """
+        update_value_str=str(update_value)
+        if isinstance(update_value, str):
+            update_value_str="'%s'"%update_value
+        """
         sql='update %s set %s='%(table_name,update_field)+ update_value + ' where '+  where_condition + ';'
         """
         sql=''
@@ -60,10 +68,10 @@ def form_sql(table_name,oper_type='query',select_field=None,where_condition=None
         sql='delete from %s'%table_name + condition + ';'
     else:
         pass
-    print('%s_sql=%s'%(oper_type,sql))
+    #print('%s_sql=%s'%(oper_type,sql))
     return sql
 
-def get_raw_hist_df(code_str,latest_count=None):
+def get_raw_hist_df0(code_str,latest_count=None):
     file_type='csv'
     file_name=RAW_HIST_DIR+code_str+'.'+file_type
     raw_column_list=['date','open','high','low','close','volume','rmb']
@@ -87,25 +95,43 @@ def get_raw_hist_df(code_str,latest_count=None):
     return df
 
 
-def get_raw_hist_df1(code_str,latest_count=None):
+def get_raw_hist_df(code_str,latest_count=None):
     file_type='csv'
     file_name=RAW_HIST_DIR+code_str+'.'+file_type
-    raw_column_list=['date0','open','high','low','close','volume','rmb']
-    print('file_name=',file_name)
+    raw_column_list=['date','open','high','low','close','volume','rmb']
+    #print('file_name=',file_name)
     df_0=pd.DataFrame({},columns=raw_column_list)
     try:
-        df_0=pd.read_csv(file_name,names=raw_column_list, header=0,encoding='gb2312')#'utf-8')   #for python3
+        try:
+            df=pd.read_csv(file_name,names=column_list, header=0,encoding='utf-8')
+            return df
+        except:
+            df_0=pd.read_csv(file_name,names=raw_column_list, header=0,encoding='gb2312')#'utf-8')   #for python3
+            df=df_0.ix[0:(len(df_0)-2)]
+            df['date'].astype(Timestamp)
+            last_volume=df.tail(1).iloc[0].volume
+            if int(last_volume)==0:
+                df=df[:-1]
+            df.to_csv(file_name,encoding='utf-8')
+            #column_list=['date','open','high','low','close','volume']
+            #column_list=['date','open','high','low','close','volume','rmb']
+            #df=pd.read_csv(file_name,names=column_list, header=0,encoding='utf-8')#,parse_dates=date_spec)
+            return df
+                #print(code_str,df)
+            #print(df)
+            
     except OSError as e:
-        print('OSError:',e)
+        #print('OSError:',e)
         return df_0
     #print df_0
     #delete column 'rmb' and delete the last row
     #del df_0['rmb']
-    df=df_0.ix[0:(len(df_0)-2)]  #to delete '通达信‘
-    #print df
+      #to delete '通达信‘
+    #df['date'].astype(Timestamp)
+    #rint(df.dtypes)
     
-    """"check"""
-    df= df.set_index('date0')
+    
+    #df= df.set_index('date0')
     """
     #df.index.name='date'
     this_time=datetime.datetime.now()
@@ -119,80 +145,16 @@ def get_raw_hist_df1(code_str,latest_count=None):
     hist_dir=ROOT_DIR+'/update/'
     file_name=hist_dir+code_str+'.'+file_type
     """
-    df.to_csv(file_name,encoding='utf-8')
-    #column_list=['date','open','high','low','close','volume']
-    column_list=['date0','open','high','low','close','volume','rmb']
-    time_list=['00:00:00']*len(df)
+    
+    """
+    time_list=['15:00:00']*len(df)
     df['time0']=pd.Series(time_list,index=df.index)
     #p=df.pop('code')
     #df.insert(0,'code',p)
-    date_spec = {'date': [0, 7]}
-    print(df)
-    df=pd.read_csv(file_name,names=column_list, header=0,encoding='utf-8',parse_dates=date_spec)
-    print(df)
-    return df
-def update_one_hist(code_str,stock_sql_obj,histdata_last_df):
+    date_spec = {'date': [1, 8]}
+    print('date_time',df)
     """
-    :param code_str: string type, code string_name
-    :param stock_sql_obj: StockSQL type, 
-    :param histdata_last_df: dataframe type, df from table histdata
-    :return: 
-    """
-    df=get_raw_hist_df(code_str)
-    if df.empty:
-        return
-    code_list=[code_str]*len(df)
-    df['code']=pd.Series(code_list,index=df.index)
-    p=df.pop('code')
-    df.insert(0,'code',p)
-    last_db_date=stock_sql_obj.get_last_db_date(code_str,histdata_last_df)
-    print('last_db_date',last_db_date,type(last_db_date))
-    last_db_date_str='%s' % last_db_date
-    last_db_date_str=last_db_date_str[:10]
-    print('last_db_date_str',last_db_date_str)
-    if last_db_date:
-        df=df[df.date>last_db_date_str]
-        if df.empty:
-            print('History data up-to-date for %s, no need update' % code_str)
-            return 0
-    stock_sql_obj.insert_table(df, 'histdata')
-    print(df.tail(1))
-    print(df.tail(1).iloc[0])
-    update_date=df.tail(1).iloc[0].date
-    #last_date=histdata_last_df.loc[date[-1],'date']
-    #update_date= 2015-11-20 <class 'str'>
-    print('update_date=',update_date,type(update_date))
-    stock_sql_obj.update_last_db_date(code_str,last_db_date,update_date,histdata_last_df)
-    return len(df)
-
-#get the all file source data in certain DIR
-def get_all_code(hist_dir):
-    """
-    :param hist_dir: string type, DIR of export data
-    :return: list type, code string list 
-    """
-    all_code=[]
-    for filename in os.listdir(hist_dir):#(r'ROOT_DIR+/export'):
-        code=filename[:-4]
-        if len(code)==6:
-            all_code.append(code)
-    return all_code
     
-def update_hist_data_tosql(codes):
-    """
-    :param codes: list type, code string list 
-    :return: 
-    """
-    #all_code=get_all_code(RAW_HIST_DIR)
-    #pd.DataFrame.to_sql()
-    stock_sql_obj=StockSQL()
-    #stock_sql_test()
-    #code_str='000987'
-    #code_str='002678'
-    histdata_last_df=stock_sql_obj.query_data(table='histdata_last')
-    for code_str in codes:
-        update_one_hist(code_str, stock_sql_obj,histdata_last_df)
-    print('update completed')
 
 class StockSQL(object):
     def __init__(self):
@@ -247,6 +209,8 @@ class StockSQL(object):
         :param condition: string type, like 'field_value>50'
         :return: 
         """
+        if isinstance(values, str):
+            values="'%s'"%values
         update_sql=form_sql(table_name=table,oper_type='update',update_field=fields,update_value=values,where_condition=condition)
         sql.execute(update_sql,self.engine)
         
@@ -275,10 +239,10 @@ class StockSQL(object):
         """
         :param code_str: string type, code_name
         :param histdata_last_df: dataframe type, df from table histdata
-        :return:  last_date: string type, last db date
+        :return:  last_date: pandas datetime, last db date
         """
         if histdata_last_df.empty:
-            print('histdata_last_df is empty')
+            #print('histdata_last_df is empty')
             return None
         else:
             try:
@@ -286,7 +250,7 @@ class StockSQL(object):
                 last_date=histdata_last_df.loc[code_str,'last_db_time']
                 return last_date
             except KeyError as e:
-                print('KeyError:',e)
+                #print('KeyError:',e)
                 return None
             
     def update_last_db_date(self,code_str,last_date,update_date,histdata_last_df):
@@ -299,13 +263,15 @@ class StockSQL(object):
         """
         if last_date:
             if update_date>last_date:
-                self.update_data(table=histdata_last, fields='last_db_time', values=update_date, condition="code='%s'"%code_str)
+                self.update_data(table='histdata_last', fields='last_db_time', values="%s"%update_date, condition="code='%s'"%code_str)
             else:
                 pass
-        elif update_date:
-            self.insert_data(table='histdata_last', fields='code,last_db_time', data=[[code_str,update_date]])
         else:
-            pass
+            if update_date:
+                self.insert_data(table='histdata_last', fields='code,last_db_time', data=[[code_str,update_date]])
+                #print('firstly update: last_db_time',update_date)
+            else:
+                pass
     #for chunk_df in pd.read_sql_query("SELECT * FROM today_stock", engine, chunksize=5):
     #    print(chunk_df)
 
@@ -330,11 +296,83 @@ def stock_sql_test():
     #stock_sql_obj.update_data(table, 'name', "'陈五'", condition="name='王五'")
     print('delete_data')
     stock_sql_obj.delete_data(table, "name='陈五'")
+
+def update_one_hist(code_str,stock_sql_obj,histdata_last_df):
+    """
+    :param code_str: string type, code string_name
+    :param stock_sql_obj: StockSQL type, 
+    :param histdata_last_df: dataframe type, df from table histdata
+    :return: 
+    """
+    df=get_raw_hist_df(code_str)
+    if df.empty:
+        return 0
+    code_list=[code_str]*len(df)
+    df['code']=pd.Series(code_list,index=df.index)
+    p=df.pop('code')
+    df.insert(0,'code',p)
+    last_db_date=stock_sql_obj.get_last_db_date(code_str,histdata_last_df)
+    last_db_date_str=''
+    #print('last_db_date',last_db_date,type(last_db_date))
+    #print('last_db_date_str',last_db_date_str)
+    #criteria0=df.volume>0
+    #df=df[df.volume>0]
+    if last_db_date:
+        last_db_date_str='%s' % last_db_date
+        last_db_date_str=last_db_date_str[:10]
+        #criteria1=df.date>last_db_date_str
+        df=df[df.date>last_db_date_str]
+        #print('sub df', df)
+    if df.empty:
+        #print('History data up-to-date for %s, no need update' % code_str)
+        return 0
+    stock_sql_obj.insert_table(df, 'histdata')
+    #print(df.tail(1))
+    #print(df.tail(1).iloc[0])
+    update_date=df.tail(1).iloc[0].date
+    #last_date=histdata_last_df.loc[date[-1],'date']
+    #update_date= 2015-11-20 <class 'str'>
+    #print('update_date=',update_date,type(update_date))
+    stock_sql_obj.update_last_db_date(code_str,last_db_date_str,update_date,histdata_last_df)
+    return len(df)
+
+#get the all file source data in certain DIR
+def get_all_code(hist_dir):
+    """
+    :param hist_dir: string type, DIR of export data
+    :return: list type, code string list 
+    """
+    all_code=[]
+    for filename in os.listdir(hist_dir):#(r'ROOT_DIR+/export'):
+        code=filename[:-4]
+        if len(code)==6:
+            all_code.append(code)
+    return all_code
+    
+def update_hist_data_tosql(codes):
+    """
+    :param codes: list type, code string list 
+    :return: 
+    """
+    starttime=datetime.datetime.now()
+    #all_code=get_all_code(RAW_HIST_DIR)
+    #pd.DataFrame.to_sql()
+    stock_sql_obj=StockSQL()
+    #stock_sql_test()
+    #code_str='000987'
+    #code_str='002678'
+    histdata_last_df=stock_sql_obj.query_data(table='histdata_last')
+    for code_str in codes:
+        update_one_hist(code_str, stock_sql_obj,histdata_last_df)
+    deltatime=datetime.datetime.now()-starttime
+    print('update duration=',deltatime.days*24*3600+deltatime.seconds)
+    print('update completed')
+        
     
 if __name__ == '__main__':   
     
     codes=get_all_code(RAW_HIST_DIR)
-    codes=['000987','000060','600750','600979','000875','600103','002678']
+    #codes=['000987','000060','600750','600979','000875','600103','002678']
     update_hist_data_tosql(codes)
 
     
