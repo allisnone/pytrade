fc17b93c99439628c0a61a8f69f3a9b059f751be
Administrator <Administrator@USER-20150725QL>
2015-11-27 08:07:19 +0800

    update_hist_and_db

--- pdsql.py
+++ pdsql.py
@@ -3,12 +3,13 @@ from sqlalchemy import create_engine
 import pymysql 
 import pandas as pd
 import numpy as np
-import tushare as ts
 from pandas.io import sql
-from tradeStrategy import *
 from pandas.lib import to_datetime
 from pandas.lib import Timestamp
-import datetime
+import datetime,time,os
+ROOT_DIR='E:/work/stockAnalyze'
+RAW_HIST_DIR=ROOT_DIR+'/export/'  
+HIST_DIR=ROOT_DIR+'/update/'
 
 def form_sql(table_name,oper_type='query',select_field=None,where_condition=None,insert_field=None,update_field=None,update_value=None):
     """
@@ -102,200 +103,27 @@ def get_raw_hist_df(code_str,latest_count=None):
     #print('file_name=',file_name)
     df_0=pd.DataFrame({},columns=raw_column_list)
     try:
-        try:
-            df=pd.read_csv(file_name,names=column_list, header=0,encoding='utf-8')
+        df=pd.read_csv(file_name,names=raw_column_list, header=0,encoding='gb2312')#'utf-8')   #for python3
+        #print('pd.read_csv=',df)
+        if df.empty:
+            #print('code_str=',code_str)
             return df
-        except:
-            df_0=pd.read_csv(file_name,names=raw_column_list, header=0,encoding='gb2312')#'utf-8')   #for python3
-            df=df_0.ix[0:(len(df_0)-2)]
-            df['date'].astype(Timestamp)
+        last_date=df.tail(1).iloc[0].date
+        if last_date=='数据来源:通达信':
+            df=df[:-1]
+            #print('数据来源:通达信')
+            #print(df.tail(1).iloc[0].date)
             last_volume=df.tail(1).iloc[0].volume
             if int(last_volume)==0:
                 df=df[:-1]
+            df['date'].astype(Timestamp)
             df.to_csv(file_name,encoding='utf-8')
-            #column_list=['date','open','high','low','close','volume']
-            #column_list=['date','open','high','low','close','volume','rmb']
-            #df=pd.read_csv(file_name,names=column_list, header=0,encoding='utf-8')#,parse_dates=date_spec)
-            return df
-                #print(code_str,df)
-            #print(df)
-            
+        else:
+            pass
+        return df
     except OSError as e:
         #print('OSError:',e)
         return df_0
-    #print df_0
-    #delete column 'rmb' and delete the last row
-    #del df_0['rmb']
-      #to delete '通达信‘
-    #df['date'].astype(Timestamp)
-    #rint(df.dtypes)
-    
-    
-    #df= df.set_index('date0')
-    """
-    #df.index.name='date'
-    this_time=datetime.datetime.now()
-    this_time_str=this_time.strftime('%Y-%m-%d %X')
-    df.index.name=this_time_str
-    hist_dir=ROOT_DIR+'/hist/'
-    file_type='csv'
-    file_name=hist_dir+code_str+'.'+file_type
-    #print df
-    df.to_csv(file_name)
-    hist_dir=ROOT_DIR+'/update/'
-    file_name=hist_dir+code_str+'.'+file_type
-    """
-    
-    """
-    time_list=['15:00:00']*len(df)
-    df['time0']=pd.Series(time_list,index=df.index)
-    #p=df.pop('code')
-    #df.insert(0,'code',p)
-    date_spec = {'date': [1, 8]}
-    print('date_time',df)
-    """
-    
-
-class StockSQL(object):
-    def __init__(self):
-        self.engine = create_engine('mysql+pymysql://emsadmin:Ems4you@112.74.101.126/stock?charset=utf8')#,encoding='utf-8',echo=True,convert_unicode=True)
-    
-    def get_table_df(self,table,columns=None):
-        """
-        :param table: string type, db_name.table_name
-        :param columns: lit type with string value, like: ['acc_name', 'initial']
-        :return: DataFrame type
-        """
-        if columns:
-            return pd.read_sql_table(table, self.engine)
-        else:
-            return pd.read_sql_table(table, self.engine, columns)
-        
-    def insert_table(self,data_frame,table_name):
-        """
-        :param data_frame: DataFrame type
-        :param table_name: string type, name of table
-        :return: 
-        """
-        data_frame.to_sql(table_name, self.engine, index=False,if_exists='append')#encoding='utf-8')
-        return
-    
-    def query_data(self,table,fields=None,condition=None):
-        """
-        :param table: string type, db_name.table_name
-        :param fields: string type, like 'id,type,value'
-        :param condition: string type, like 'field_value>50'
-        :return: DataFrame type
-        """
-        query_sql=form_sql(table_name=table, oper_type='query', select_field=fields, where_condition=condition)
-        return pd.read_sql_query(query_sql, self.engine)
-    
-    def insert_data(self,table,fields,data):
-        """
-        :param table: string type, db_name.table_name
-        :param fields: string type, like 'id,type,value'
-        :param data: list type of list value, like: data=[['李5'],['黄9']]
-        :return: 
-        """
-        fields='(' + fields +')'
-        insert_sql=form_sql(table_name=table,oper_type='insert',insert_field=fields)
-        sql.execute(insert_sql, self.engine, params=data)
-        
-    def update_data(self,table,fields,values,condition=None):
-        """
-        :param table: string type, db_name.table_name
-        :param fields: string type, like 'value'
-        :param values: string type, like:  '1000' (for value type) or "'normal'" (for string type)
-        :param condition: string type, like 'field_value>50'
-        :return: 
-        """
-        if isinstance(values, str):
-            values="'%s'"%values
-        update_sql=form_sql(table_name=table,oper_type='update',update_field=fields,update_value=values,where_condition=condition)
-        sql.execute(update_sql,self.engine)
-        
-    def delete_data(self,table,condition=None):
-        """
-        :param table_name: string type, db_name.table_name
-        :param condition: string type, like 'field_value>50'
-        :return: 
-        """
-        delete_sql=form_sql(table_name=table,oper_type='delete',where_condition=condition)
-        sql.execute(delete_sql, self.engine)
-    
-    def get_last_db_date0(self,code_str):
-        code_condition="code='%s'" % code_str
-        last_df=self.query_data(table='histdata', fields='date', condition=code_condition + ' order by date desc limit 1')
-        print(last_df)
-        if last_df.empty:
-            return ''
-        else:
-            last_date=last_df.ix[0].date
-            print(type(last_date))
-            print('last_date=',last_date)
-            return last_date
-    
-    def get_last_db_date(self,code_str,histdata_last_df):
-        """
-        :param code_str: string type, code_name
-        :param histdata_last_df: dataframe type, df from table histdata
-        :return:  last_date: pandas datetime, last db date
-        """
-        if histdata_last_df.empty:
-            #print('histdata_last_df is empty')
-            return None
-        else:
-            try:
-                histdata_last_df=histdata_last_df.set_index('code')
-                last_date=histdata_last_df.loc[code_str,'last_db_time']
-                return last_date
-            except KeyError as e:
-                #print('KeyError:',e)
-                return None
-            
-    def update_last_db_date(self,code_str,last_date,update_date,histdata_last_df):
-        """
-        :param code_str: string type, code_name
-        :param last_date: string type, last db date
-        :param update_date: string type, this date 
-        :param histdata_last_df: dataframe type, df from table histdata
-        :return: 
-        """
-        if last_date:
-            if update_date>last_date:
-                self.update_data(table='histdata_last', fields='last_db_time', values="%s"%update_date, condition="code='%s'"%code_str)
-            else:
-                pass
-        else:
-            if update_date:
-                self.insert_data(table='histdata_last', fields='code,last_db_time', data=[[code_str,update_date]])
-                #print('firstly update: last_db_time',update_date)
-            else:
-                pass
-    #for chunk_df in pd.read_sql_query("SELECT * FROM today_stock", engine, chunksize=5):
-    #    print(chunk_df)
-
-def stock_sql_test():
-    stock_sql_obj=StockSQL()
-    table='test'
-    df=stock_sql_obj.get_table_df(table)#, columns=['name']) 
-    print('get_table_df=')
-    print(df)
-    df2=stock_sql_obj.get_table_df(table, columns=['name']) 
-    print(df2)
-    print('query_data:')
-    df3=stock_sql_obj.query_data(table)
-    print(df3)
-    df3=stock_sql_obj.query_data(table, 'name', "name='王五'")
-    print(df3)
-    print('insert_data:')
-    data=[['李二'],['黄三']]
-    stock_sql_obj.insert_data(table, 'name', data)
-    print('update_data:')
-    stock_sql_obj.update_data(table, 'name', "'陈五'", condition="name='王五'")
-    #stock_sql_obj.update_data(table, 'name', "'陈五'", condition="name='王五'")
-    print('delete_data')
-    stock_sql_obj.delete_data(table, "name='陈五'")
 
 def update_one_hist(code_str,stock_sql_obj,histdata_last_df):
     """
@@ -368,11 +196,150 @@ def update_hist_data_tosql(codes):
     print('update duration=',deltatime.days*24*3600+deltatime.seconds)
     print('update completed')
         
+def is_trade_time_now():
+    except_trade_day_list=['2015-05-01','2015-06-22','2015-09-03','2015-10-01','2015-10-02','2015-10-06','2015-10-07','2015-10-08']
+    now_timestamp=time.time()
+    this_time=datetime.datetime.now()
+    this_date=this_time.strftime('%Y-%m-%d')
+    hour=this_time.hour
+    minute=this_time.minute
+    is_trade_time=((hour>=9 and minute>=30) and (hour<=11 and minute<=30)) or (hour>=13 and hour<=15)
+    is_working_date=this_time.isoweekday()<6 and (this_date not in except_trade_day_list)
+    return is_trade_time and is_working_date
+
+
+class StockSQL(object):
+    def __init__(self):
+        self.engine = create_engine('mysql+pymysql://emsadmin:Ems4you@112.74.101.126/stock?charset=utf8')#,encoding='utf-8',echo=True,convert_unicode=True)
     
-if __name__ == '__main__':   
+    def get_table_df(self,table,columns=None):
+        """
+        :param table: string type, db_name.table_name
+        :param columns: lit type with string value, like: ['acc_name', 'initial']
+        :return: DataFrame type
+        """
+        if columns:
+            return pd.read_sql_table(table, self.engine)
+        else:
+            return pd.read_sql_table(table, self.engine, columns)
+        
+    def insert_table(self,data_frame,table_name):
+        """
+        :param data_frame: DataFrame type
+        :param table_name: string type, name of table
+        :return: 
+        """
+        data_frame.to_sql(table_name, self.engine, index=False,if_exists='append')#encoding='utf-8')
+        return
     
-    codes=get_all_code(RAW_HIST_DIR)
-    #codes=['000987','000060','600750','600979','000875','600103','002678']
-    update_hist_data_tosql(codes)
+    def query_data(self,table,fields=None,condition=None):
+        """
+        :param table: string type, db_name.table_name
+        :param fields: string type, like 'id,type,value'
+        :param condition: string type, like 'field_value>50'
+        :return: DataFrame type
+        """
+        query_sql=form_sql(table_name=table, oper_type='query', select_field=fields, where_condition=condition)
+        return pd.read_sql_query(query_sql, self.engine)
+    
+    def insert_data(self,table,fields,data):
+        """
+        :param table: string type, db_name.table_name
+        :param fields: string type, like 'id,type,value'
+        :param data: list type of list value, like: data=[['李5'],['黄9']]
+        :return: 
+        """
+        fields='(' + fields +')'
+        insert_sql=form_sql(table_name=table,oper_type='insert',insert_field=fields)
+        sql.execute(insert_sql, self.engine, params=data)
+        
+    def update_data(self,table,fields,values,condition=None):
+        """
+        :param table: string type, db_name.table_name
+        :param fields: string type, like 'value'
+        :param values: string type, like:  '1000' (for value type) or "'normal'" (for string type)
+        :param condition: string type, like 'field_value>50'
+        :return: 
+        """
+        if isinstance(values, str):
+            values="'%s'"%values
+        update_sql=form_sql(table_name=table,oper_type='update',update_field=fields,update_value=values,where_condition=condition)
+        sql.execute(update_sql,self.engine)
+        
+    def delete_data(self,table,condition=None):
+        """
+        :param table_name: string type, db_name.table_name
+        :param condition: string type, like 'field_value>50'
+        :return: 
+        """
+        delete_sql=form_sql(table_name=table,oper_type='delete',where_condition=condition)
+        sql.execute(delete_sql, self.engine)
+    
+    def get_last_db_date(self,code_str,histdata_last_df):
+        """
+        :param code_str: string type, code_name
+        :param histdata_last_df: dataframe type, df from table histdata
+        :return:  last_date: pandas datetime, last db date
+        """
+        if histdata_last_df.empty:
+            #print('histdata_last_df is empty')
+            return None
+        else:
+            try:
+                histdata_last_df=histdata_last_df.set_index('code')
+                last_date=histdata_last_df.loc[code_str,'last_db_time']
+                return last_date
+            except KeyError as e:
+                #print('KeyError:',e)
+                return None
+            
+    def update_last_db_date(self,code_str,last_date,update_date,histdata_last_df):
+        """
+        :param code_str: string type, code_name
+        :param last_date: string type, last db date
+        :param update_date: string type, this date 
+        :param histdata_last_df: dataframe type, df from table histdata
+        :return: 
+        """
+        if last_date:
+            if update_date>last_date:
+                self.update_data(table='histdata_last', fields='last_db_time', values="%s"%update_date, condition="code='%s'"%code_str)
+            else:
+                pass
+        else:
+            if update_date:
+                self.insert_data(table='histdata_last', fields='code,last_db_time', data=[[code_str,update_date]])
+                #print('firstly update: last_db_time',update_date)
+            else:
+                pass
+    
+    
+    #for chunk_df in pd.read_sql_query("SELECT * FROM today_stock", engine, chunksize=5):
+    #    print(chunk_df)
+
+def stock_sql_test():
+    stock_sql_obj=StockSQL()
+    table='test'
+    df=stock_sql_obj.get_table_df(table)#, columns=['name']) 
+    print('get_table_df=')
+    print(df)
+    df2=stock_sql_obj.get_table_df(table, columns=['name']) 
+    print(df2)
+    print('query_data:')
+    df3=stock_sql_obj.query_data(table)
+    print(df3)
+    df3=stock_sql_obj.query_data(table, 'name', "name='王五'")
+    print(df3)
+    print('insert_data:')
+    data=[['李二'],['黄三']]
+    stock_sql_obj.insert_data(table, 'name', data)
+    print('update_data:')
+    stock_sql_obj.update_data(table, 'name', "'陈五'", condition="name='王五'")
+    #stock_sql_obj.update_data(table, 'name', "'陈五'", condition="name='王五'")
+    print('delete_data')
+    stock_sql_obj.delete_data(table, "name='陈五'")
+
+
+
 
     
