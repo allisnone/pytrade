323234622fdb82a0a92ea8ec7fea4e3c00226872
ezhguox <ezhguox@CN00103287.ericsson.se>
2016-02-26 15:06:39 +0800

    autotrade

---
+++ autotrade.py
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+
+def get_strategy():
+    return
+
+def get_account_info():
+    return
+
+def get_market_score():
+    return
+
+def determine_suit_strategy():
+    return
+
+def filter_by_strategy(strategy_id):
+    return
+
+def get_loss_exit():
+    return
+
+def caculate_dynamic_loss_exit():
+    return
+
+def get_profile_exit():
+    return
+
+def holding_monitor():
+    return
+
+def new_buy_monitor():
+    return
+
+def update_trade():
+    return
+
+def update_account():
+    return
+
+def calculte_max_dropdown():
+    return
