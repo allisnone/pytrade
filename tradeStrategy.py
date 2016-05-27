50904664ded0042c7d2422ffd5e084d7846ac7a3
ezhguox <ezhguox@CN00103287.ericsson.se>
2016-05-27 15:48:40 +0800

    u

--- tradeStrategy.py
+++ tradeStrategy.py
@@ -1857,7 +1857,7 @@ class Stockhistory:
         self.temp_hist_df['b_price0'] = np.where(self.temp_hist_df['close']>self.temp_hist_df['c_max3'].shift(1),self.temp_hist_df['c_max3'].shift(1),0)
         self.temp_hist_df['b_price'] = np.where(self.temp_hist_df['low']>self.temp_hist_df['b_price0'],-self.temp_hist_df['low'], -self.temp_hist_df['b_price0'])
         
-        print(self.temp_hist_df[['s_price', 'b_price']].tail(2))
+        print(self.temp_hist_df[['s_price', 'b_price']].tail(30))
         self.temp_hist_df['b_price'] = np.where(((self.temp_hist_df['b_price'].shift(1)==0) 
                                                 & (self.temp_hist_df['s_price']==0)
                                                 & (self.temp_hist_df['b_price']<0)), self.temp_hist_df['b_price'], 0)
