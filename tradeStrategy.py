c068d2afdb3f05e07512519cc337b1f80e6c63eb
ezhguox <ezhguox@CN00103287.ericsson.se>
2016-05-27 17:41:58 +0800

    update

--- tradeStrategy.py
+++ tradeStrategy.py
@@ -1888,12 +1888,12 @@ class Stockhistory:
         #temp_df = self.temp_hist_df[['date','close','p_change', 'position','operation','s_price','b_price']]
         print(temp_df.describe())
         print(temp_df.sum())
-        summary = temp_df.describe()
+        summary_profit = temp_df.describe()['profit']
         trade_times = len(temp_df)/2
         TRADE_FEE = 0.00162
         total_profit = temp_df.sum().profit - trade_times * TRADE_FEE
-        summary['sum_profit'] = total_profit
-        print(summary)
+        summary_profit['sum'] = total_profit
+        print(summary_profit)
         temp_df.to_csv('./temp/bs_%s.csv' % self.code)
         return
     
