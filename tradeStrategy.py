80e1841ec865531c4da742b8619ec56f6602bdf7
ezhguox <ezhguox@CN00103287.ericsson.se>
2016-05-30 11:41:15 +0800

    u

--- tradeStrategy.py
+++ tradeStrategy.py
@@ -1551,7 +1551,8 @@ class Stockhistory:
             temp_df['c_max3'] = temp_df['close'].rolling(window=3,center=False).max().round(2)
             temp_df['l_min3'] = temp_df['low'].rolling(window=3,center=False).min().round(2)
             temp_df['c_min2'] = temp_df['close'].rolling(window=2,center=False).min().round(2)
-            temp_df['id_c_max20'] = temp_df['close'].idxmax(axis=0)
+            #temp_df['id_c_max20'] = temp_df['close'].idxmax(axis=0)
+            temp_df['id_c_max20'] = temp_df['close'].rolling(window=20,center=False).apply(pd.Series.idxmax(axis=0))
         else:#elif '3.4' in platform.python_version():
             temp_df['atr_ma%s'%short_num] = pd.rolling_mean(temp_df['atr'], window=short_num).round(2)
             temp_df['atr_ma%s'%long_num] = pd.rolling_mean(temp_df['atr'], window=long_num).round(2)
