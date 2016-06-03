cc51c8de4f99b71af12b28b555d893a7e6b5455d
Administrator <Administrator@USER-20150725QL>
2016-06-03 08:45:39 +0800

    u

--- myBackTest.py
+++ myBackTest.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     trend_column_list = ['count', 'mean','chg_fuli', 'std', 'min', '25%', '50%', '75%', 'max', 'c_state',
                         'c_mean', 'pos_mean', 'ft_rate', 'presure', 'holding', 'close','cont_num']
     all_trend_result_df = tds.pd.DataFrame({}, columns=trend_column_list)
-    all_codes = ['300128', '002288', '002156', '002799']# '300476', '002548', '002799']
+    #all_codes = ['300128', '002288', '002156', '002799']# '300476', '002548', '002799']
     ma_num = 20
     for stock_synbol in all_codes:
         s_stock=tds.Stockhistory(stock_synbol,'D',test_num=num)
