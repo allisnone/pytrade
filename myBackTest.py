460e112c5ec8b76d0411505e07faf67f8e0aa6d8
ezhguox <ezhguox@CN00103287.ericsson.se>
2016-06-08 18:06:50 +0800

    u

--- myBackTest.py
+++ myBackTest.py
@@ -50,9 +50,9 @@ if __name__ == "__main__":
     all_codes = pds.get_all_code(pds.RAW_HIST_DIR)
     i=0
     trend_column_list = ['count', 'mean','chg_fuli', 'std', 'min', '25%', '50%', '75%', 'max', 'c_state',
-                        'c_mean', 'pos_mean', 'ft_rate', 'presure', 'holding', 'close','cont_num']
+                        'c_mean', 'pos_mean', 'ft_rate', 'presure', 'holding', 'close','cont_num','rmb_rate','ma_rmb_rate']
     all_trend_result_df = tds.pd.DataFrame({}, columns=trend_column_list)
-    #all_codes = ['300128', '002288', '002156', '300126','300162','002717','002799']# '300476', '002548', '002799']
+    all_codes = ['300128', '002288', '002156', '300126','300162','002717','002799']# '300476', '002548', '002799']
     ma_num = 20
     for stock_synbol in all_codes:
         s_stock=tds.Stockhistory(stock_synbol,'D',test_num=num)
@@ -109,7 +109,8 @@ if __name__ == "__main__":
     all_result_df['max_r'] = all_result_df['max']/all_result_df['cum_prf']
     ma_c_name = '%s日趋势数' % ma_num
     trend_column_chiness = {'count':ma_c_name, 'mean': '平均涨幅','chg_fuli': '复利涨幅', 'std': '标准差', 'min': '最小涨幅', '25%': '25%', '50%': '50%', '75%': '75%', 'max': '最大涨幅', 'c_state': '收盘价状态',
-                        'c_mean': '平均收盘价', 'pos_mean': '平均仓位', 'ft_rate': '低点反弹率', 'presure': '压力', 'holding': '支撑', 'close': '收盘价','cont_num': '连涨天数', 'name': '名字', 'stopped': '停牌','invalid': '除外'}
+                        'c_mean': '平均收盘价', 'pos_mean': '平均仓位', 'ft_rate': '低点反弹率', 'presure': '压力', 'holding': '支撑', 'close': '收盘价','cont_num': '连涨天数', 'name': '名字', 'stopped': '停牌','invalid': '除外',
+                        'rmb_rate':'量比','ma_rmb_rate':'短长量比'}
     all_trend_result_df_chinese = all_trend_result_df.rename(index=str, columns=trend_column_chiness)
     print(all_result_df)
     #print(all_trend_result_df)
