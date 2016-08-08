b708e8030e76eb9ca1854dfc5b416bf855fcbf36
ezhguox <ezhguox@CN00103287.ericsson.se>
2016-08-08 15:33:07 +0800

    u

--- update_history_k_data.py
+++ update_history_k_data.py
@@ -40,7 +40,7 @@ if __name__ == "__main__":
                     """更新持仓信息到数据库"""
                     stock_sql.update_sql_position()
                     """更新指数历史数据到数据库"""
-                    stock_sql.update_sql_index(force_update=True)#,'sz','zxb','cyb','hs300','sh50'])
+                    #stock_sql.update_sql_index(force_update=True)#,'sz','zxb','cyb','hs300','sh50'])
                     sleep_seconds=30
             else:
                 if datetime.datetime.now().hour==18:
@@ -54,7 +54,7 @@ if __name__ == "__main__":
                     """更新持仓信息到数据库"""
                     stock_sql.update_sql_position()
                     """更新指数历史数据到数据库"""
-                    stock_sql.update_sql_index(force_update=True)#,'sz','zxb','cyb','hs300','sh50'])
+                    #stock_sql.update_sql_index(force_update=True)#,'sz','zxb','cyb','hs300','sh50'])
                     sleep_seconds = tt.get_remain_time_to_trade()
                 else:
                     pass
