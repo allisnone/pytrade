6a82d22fa1a0e151a85d5df735b7077aaf66a0f0
Administrator <Administrator@USER-20150725QL>
2016-08-12 08:03:08 +0800

    u

--- update_history_k_data.py
+++ update_history_k_data.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                        '002544','300395','002605','300403','002225','002297','600572','000333',
                        '300413','002285','002312','002509','600305','002631','603718','002496',
                        '002600','603198','002444','300238','300467','300028','300033','300126',
-                       '300135','300143','300380','300399','300117']
+                       '300135','300143','300380','300399','300117','000029']
     #given_codes = ['300128', '002288', '002156', '300126','300162','002717','002799','300515','300516','600519',
     #               '000418','002673','600060','600887','000810','600115','600567','600199','000596','000538','002274','600036','600030','601398']
     given_codes = []                
@@ -45,9 +45,10 @@ if __name__ == "__main__":
             else:
                 if datetime.datetime.now().hour==18:
                     #easyhistory.init('D', export='csv', path="C:/hist")
+                    updated_date_count = updated_date_count +1
                     """更新股票数据"""
                     easyhistory.update(path="C:/hist",stock_codes=given_codes)
-                    updated_date_count = updated_date_count +1
+                    
                     print('update count %s at: ' % updated_date_count, datetime.datetime.now() )
                     mybt.back_test(k_num=date_str, given_codes=[],except_stocks=except_stocks)
                     
