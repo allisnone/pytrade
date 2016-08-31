15dd284f3dbb96f956d8df4b00e866d7272c00c5
Administrator <Administrator@USER-20150725QL>
2016-08-31 22:33:43 +0800

    u7

--- tradeStrategy.py
+++ tradeStrategy.py
@@ -2121,6 +2121,7 @@ class Stockhistory:
         if  isinstance(self.test_num, int):
             temp_hist_df =self.temp_hist_df.tail(self.test_num)
         elif isinstance(self.test_num, str):
+            print(self.test_num)
             temp_hist_df =self.temp_hist_df[self.temp_hist_df['date']>self.test_num]
         else:
             pass
