37ba480bfb6e26a5ce07ae3b110f9541b4e967a3
ezhguox <ezhguox@CN00103287.ericsson.se>
2017-01-19 16:47:45 +0800

    u

--- pybuyer.py
+++ pybuyer.py
@@ -10,5 +10,6 @@ stock_sql = StockSQL()
 print(datetime.datetime.now())
 accounts = ['36005', '38736']
 debug_enable = True
+
 op_tdx = OperationTdx(debug=debug_enable)
 get_acc_buy_stocks(op_tdx,stock_sql,acc_list=accounts, buy_rate=1.0)
