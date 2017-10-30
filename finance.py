2d6d6fd9de2da30765fd29820d701f318a4bd7b6
ezhguox <ezhguox@CN00103287.mshome.net>
2017-10-30 12:06:09 +0800

    update

--- finance.py
+++ finance.py
@@ -4,6 +4,5 @@ basic_df = ts.get_stock_basics()
 basic_df = basic_df.sort_index(axis=0, by='totalAssets', ascending=False)
 print(basic_df)
 #profit = ts.get_profit_data(2017,2)
-
 #print(profit)
 #ts.get_profit_statement()
